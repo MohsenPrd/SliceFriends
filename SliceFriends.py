7db1d03cc1a1c529abd48856d06d3f78648248de
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2023-12-03 20:43:27 +0330

    Create SliceFriends.py

---
+++ SliceFriends.py
@@ -0,0 +1,10 @@
+## Mohsen Pourdehghan
+
+friends= ['mohammadreza', 'hossein', 'amirmohammad', 'sina', 'mohammad', 'yasin', 'mehdi']
+
+f1 = friends[0:3]
+f2 = friends[0:4:2]
+f3 = friends[0:-1:3]
+print(f1)
+print(f2)
+print(f3)
